2ae477d029a388caabc89ef39812058f50c1dea9
Doug Woos <doug.woos@gmail.com>
2016-01-08 12:24:01 -0800

    move nth to util

--- core/Net.v
+++ core/Net.v
@@ -408,33 +408,6 @@ Class SimpleUpdateParams `(P : FailureParams) :=
     updates : list (handlerUpdate P)
   }.
 
-Inductive Nth {A : Type} : list A -> nat -> A -> Prop :=
-| Nth_0 : forall x l, Nth (x :: l) 0 x
-| Nth_S : forall l x n y, Nth l n x -> Nth (y :: l) (S n) x.
-
-Lemma nth_error_Nth :
-  forall A n l (x : A),
-    nth_error l n = Some x ->
-    Nth l n x.
-Proof.
-  induction n; intros; simpl in *; auto.
-  - break_match; try discriminate.
-    unfold value in *.
-    find_inversion.
-    constructor.
-  - break_match; try discriminate.
-    subst. constructor.
-    eauto.
-Qed.
-
-Lemma Nth_nth_error :
-  forall A n l (x : A),
-    Nth l n x ->
-    nth_error l n = Some x.
-Proof.
-  intros.
-  induction H; simpl in *; auto.
-Qed.
 
 Section StepSimpleUpdate.
   Context `{params : SimpleUpdateParams}.
